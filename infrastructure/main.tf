ea2f53653c8534b51408eeb035ca37cd758c3ef5
Georgi Stavrev <georgi@insurify.com>
2021-03-20 18:07:07 +0200

    formats terraform files

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -7,20 +7,20 @@ terraform {
 }
 
 provider "aws" {
-   region = "eu-west-1"
-   access_key = var.aws_access_key
-   secret_key = var.aws_secret_key
+  region     = "eu-west-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
 
 resource "aws_s3_bucket" "moggiez_lambdas" {
-    bucket = "moggiez-lambdas"
-    acl    = "private"
+  bucket = "moggiez-lambdas"
+  acl    = "private"
 
-    tags = {
-        Project     = "Eventest"
-        Name        = "eventest"
-        Environment = "Prod"
-    }
+  tags = {
+    Project     = "Eventest"
+    Name        = "eventest"
+    Environment = "Prod"
+  }
 }
 
 resource "aws_s3_bucket_object" "worker_lambda_s3_object" {
@@ -28,7 +28,7 @@ resource "aws_s3_bucket_object" "worker_lambda_s3_object" {
   key    = "worker.lambda.${var.lambda_version}.zip"
   acl    = "private"
   source = "${var.dist_dir}/worker.lambda.${var.lambda_version}.zip"
-  etag = filemd5("${var.dist_dir}/worker.lambda.${var.lambda_version}.zip")
+  etag   = filemd5("${var.dist_dir}/worker.lambda.${var.lambda_version}.zip")
 }
 
 resource "aws_s3_bucket_object" "driver_lambda_s3_object" {
@@ -36,31 +36,31 @@ resource "aws_s3_bucket_object" "driver_lambda_s3_object" {
   key    = "driver.lambda.${var.lambda_version}.zip"
   acl    = "private"
   source = "${var.dist_dir}/driver.lambda.${var.lambda_version}.zip"
-  etag = filemd5("${var.dist_dir}/driver.lambda.${var.lambda_version}.zip")
+  etag   = filemd5("${var.dist_dir}/driver.lambda.${var.lambda_version}.zip")
 }
 
 resource "aws_lambda_function" "moggiez_worker_fn" {
-   function_name = "MoggiezWorker"
-   s3_bucket = aws_s3_bucket.moggiez_lambdas.bucket
-   s3_key = aws_s3_bucket_object.worker_lambda_s3_object.key
+  function_name = "MoggiezWorker"
+  s3_bucket     = aws_s3_bucket.moggiez_lambdas.bucket
+  s3_key        = aws_s3_bucket_object.worker_lambda_s3_object.key
 
-   handler = "worker.handler"
-   runtime = "nodejs14.x"
-   source_code_hash = filebase64sha256("${var.dist_dir}/worker.lambda.${var.lambda_version}.zip")
+  handler          = "worker.handler"
+  runtime          = "nodejs14.x"
+  source_code_hash = filebase64sha256("${var.dist_dir}/worker.lambda.${var.lambda_version}.zip")
 
-   role = aws_iam_role.lambda_exec.arn
+  role = aws_iam_role.lambda_exec.arn
 }
 
 resource "aws_lambda_function" "moggiez_driver_fn" {
-   function_name = "MoggiezDriver"
-   s3_bucket = aws_s3_bucket.moggiez_lambdas.bucket
-   s3_key = aws_s3_bucket_object.driver_lambda_s3_object.key
+  function_name = "MoggiezDriver"
+  s3_bucket     = aws_s3_bucket.moggiez_lambdas.bucket
+  s3_key        = aws_s3_bucket_object.driver_lambda_s3_object.key
 
-   handler = "driver.handler"
-   runtime = "nodejs14.x"
-   source_code_hash = filebase64sha256("${var.dist_dir}/driver.lambda.${var.lambda_version}.zip")
+  handler          = "driver.handler"
+  runtime          = "nodejs14.x"
+  source_code_hash = filebase64sha256("${var.dist_dir}/driver.lambda.${var.lambda_version}.zip")
 
-   role = aws_iam_role.lambda_exec.arn
+  role = aws_iam_role.lambda_exec.arn
 }
 
 resource "aws_iam_policy" "eventbridge_events" {
@@ -69,31 +69,31 @@ resource "aws_iam_policy" "eventbridge_events" {
   description = "IAM policy for logging from a lambda"
 
   policy = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
+    "Version" : "2012-10-17",
+    "Statement" : [
       {
-        "Effect": "Allow",
-        "Action": "events:PutEvents",
-        "Resource": "*"
+        "Effect" : "Allow",
+        "Action" : "events:PutEvents",
+        "Resource" : "*"
       }
     ]
   })
 }
 
- # IAM role which dictates what other AWS services the Lambda function
- # may access.
+# IAM role which dictates what other AWS services the Lambda function
+# may access.
 resource "aws_iam_role" "lambda_exec" {
   name = "moggiez_lambda_execution_role"
   assume_role_policy = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
+    "Version" : "2012-10-17",
+    "Statement" : [
       {
-        "Action": "sts:AssumeRole",
-        "Principal": {
-          "Service": "lambda.amazonaws.com"
+        "Action" : "sts:AssumeRole",
+        "Principal" : {
+          "Service" : "lambda.amazonaws.com"
         },
-        "Effect": "Allow",
-        "Sid": ""
+        "Effect" : "Allow",
+        "Sid" : ""
       }
     ]
   })
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,19 +1,19 @@
 variable "aws_access_key" {
-    type = string
-    sensitive   = true
+  type      = string
+  sensitive = true
 }
 
 variable "aws_secret_key" {
-    type = string
-    sensitive = true
+  type      = string
+  sensitive = true
 }
 
 variable "lambda_version" {
-  type = string
+  type    = string
   default = "1.0.0"
 }
 
 variable "dist_dir" {
-  type = string
+  type    = string
   default = "../dist"
 }
