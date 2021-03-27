62d1ec3a837f34158414de4e56fa2205dd3dd204
Georgi Stavrev <stavrev.georgi@gmail.com>
2021-03-27 19:26:36 +0200

    fixes log groups with same name

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -29,11 +29,11 @@ locals {
 }
 
 resource "aws_cloudwatch_log_group" "moggiez_worker" {
-  name = "/aws/events/moggiez_test"
+  name = "/aws/events/moggiez_worker"
 }
 
 resource "aws_cloudwatch_log_group" "moggiez_driver" {
-  name = "/aws/events/moggiez_test"
+  name = "/aws/events/moggiez_driver"
 }
 
 resource "aws_cloudwatch_event_bus" "moggiez_load_test" {
